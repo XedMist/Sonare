6190541a424253468c0d6d9c9b4cd8f3faffc43d
XedMist <xedgames7@gmail.com>
2025-10-01 11:22:23 +0200

    Drizzle SQLITE DB

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,28 +1,30 @@
-import type { User } from "../model/User.ts";
-
-// Si fuera de verdad se deberia llamar a la base de datos
-// Para la prueba se utiliza un mapa
-let nextId = 1;
-const store = new Map<number, User>();
+import { type User, userSchema } from "../model/User.ts";
+import { InsertUser, usersTable } from "../db/schema.ts";
+import { eq } from "drizzle-orm";
+import { db } from "../db/db.ts";
 
 export default class UserRepository {
-  // No hace falta el async pero lo dejo para imitar a como se
-  // haria con una base de datos real
   async findAll(): Promise<User[]> {
-    return Array.from(store.values());
+    return await db.select().from(usersTable);
   }
 
   async insert(u: Omit<User, "id">): Promise<User> {
-    const user: User = { id: nextId++, ...u };
-    store.set(user.id, user);
-    return user;
+    const newUser: InsertUser = { name: u.name };
+    const user = await db.insert(usersTable).values(newUser).returning();
+    return userSchema.parse(user[0]);
   }
 
   async findById(id: number): Promise<User | null> {
-    return store.get(id) || null;
+    const user = await db.select().from(usersTable).where(
+      eq(usersTable.id, id),
+    );
+
+    return user.length == 0 ? null : userSchema.parse(user[0]);
   }
 
   async delete(id: number): Promise<boolean> {
-    return store.delete(id);
+    const rs = await db.delete(usersTable).where(eq(usersTable.id, id))
+      .returning();
+    return rs.length != 0;
   }
 }

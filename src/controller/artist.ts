1b48d60ceeedd3c8905e326db75a5a93bed3c1af
XedMist <xedgames7@gmail.com>
2025-10-15 09:17:27 +0200

    QF

--- src/controller/artist.ts
+++ src/controller/artist.ts
@@ -11,8 +11,6 @@ router.get("/", async (c) => {
   return c.json(artists);
 });
 
-// El zValidator comprueba que lo que se pase en el curpo de la
-// peticion es lo que se espera. El omit es para que no pasen la id
 router.post(
   "/",
   zValidator("json", artistSchema.omit({ id: true })),
@@ -23,27 +21,5 @@ router.post(
     return c.json(created, 201);
   },
 );
-
-router.get("/:id", async (c) => {
-  const { id } = c.req.param();
-  const artist = await service.findById(Number(id));
-
-  if (artist) {
-    return c.json(artist);
-  } else {
-    return c.json({ message: "Artist not found" }, 404);
-  }
-});
-
-router.delete("/:id", async (c) => {
-  const { id } = c.req.param();
-  const artist = await service.delete(Number(id));
-
-  if (artist) {
-    return c.json({ message: "Artist deleted" });
-  } else {
-    return c.json({ message: "Artist not found" }, 404);
-  }
-});
 
 export default router;
--- src/services/ArtistService.ts
+++ src/services/ArtistService.ts
@@ -5,22 +5,10 @@ export default class ArtistService {
   repo = new ArtistRepository();
 
   async findAll(): Promise<Artist[]> {
-    return this.repo.findAll();
+    return await this.repo.findAll();
   }
 
-  // El omit es para no tener que pasar el id al crear el artista
-  // De eso se encarga el repo
   async create(payload: Omit<Artist, "id">): Promise<Artist> {
-    return this.repo.insert(payload);
-  }
-
-  async findById(id: number): Promise<Artist | null> {
-    return this.repo.findById(id);
-  }
-
-  async delete(id: number): Promise<boolean> {
-    const artist = await this.repo.findById(id);
-    if (artist) return this.repo.delete(id);
-    return false;
+    return await this.repo.insert(payload);
   }
 }

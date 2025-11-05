c760f960ecb051493e41a5f2363258e0c69a7851
XedMist <xedgames7@gmail.com>
2025-11-05 11:05:53 +0100

    Album otra vez

--- src/controller/album.ts
+++ src/controller/album.ts
@@ -1,23 +1,36 @@
 import AlbumService from "../services/AlbumService.ts";
 import { Hono } from "hono";
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
 
-const router = new Hono();
+
+const albumController = new Hono();
 const service = new AlbumService();
 
-router.get("/", async (c) => {
-    const name = c.req.query("name");
-    const artist = c.req.query("artist");
-    if (name) {
-        const albums = await service.getAlbums();
-        return c.json(albums);
-    } else if (artist) {
-        const albums = await service.getAlbumsFromArtist(Number(artist));
-        return c.json(albums);
-    } else {
-        return c.json({
-            message: "You must provide a name or artist query parameter",
-        }, 400);
-    }
+const PaginationQuerySchema = z.object({
+    page: z.string().regex(/^\d+$/).transform(Number).default(0).pipe(z.number().min(0)),
+    limit: z.string().regex(/^\d+$/).transform(Number).default(10).pipe(z.number().min(1).max(100)),
 });
 
-export default router;
+
+albumController.get("/", zValidator("query", PaginationQuerySchema), async (c) => {
+    const { page, limit } = c.req.valid('query')
+    const albums = await service.findAll({ skip: page, take: limit });
+    return c.json(albums);
+});
+
+albumController.get("/:id", async (c) => {
+    const { id } = c.req.param();
+    const album = await service.findByID(id);
+    return c.json(album);
+});
+
+albumController.get("/:id/tracks", zValidator("query", PaginationQuerySchema), async (c) => {
+    const { page, limit } = c.req.valid('query')
+    const { id } = c.req.param();
+
+    const albums = await service.getTracksOfAlbum(id, { skip: page, take: limit });
+    return c.json(albums);
+});
+
+export default albumController;
